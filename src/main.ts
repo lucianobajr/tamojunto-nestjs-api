81495e8f14de483c811537962b1292ca04a424d2
lucianobajr <luciano.alcantara@ufv.br>
2021-12-30 10:36:50 -0300

    add (+): change port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  const port = 3000 || process.env.PORT;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(`API is running on port: ${port} 🔥 !`);
 }
